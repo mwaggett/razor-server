89d339102505e5631f6c7a7edb4b1463558f2675
Daniel Pittman <daniel@rimspace.net>
2014-05-15 12:20:10 -0700

    Implement basic client command testing
    Many tests added.  See individual files for details.
    
    Signed-off-by: Daniel Pittman <daniel@rimspace.net>

---
+++ 20_client_commands/C502_create_puppet_broker_with_environment_and_server.rb
@@ -0,0 +1,25 @@
+# -*- encoding: utf-8 -*-
+# this is required because of the use of eval interacting badly with require_relative
+require File.expand_path(__FILE__ + '/../../razor_helper', '.')
+confine :to, :platform => 'el-6'
+confine :except, :roles => %w{master dashboard database}
+
+test_name "C502	Create 'puppet' Broker with environment and server"
+step "https://testrail.ops.puppetlabs.net/index.php?/cases/view/502"
+
+reset_database
+json = {
+  "name" => "puppet-broker-test",
+  "broker-type" => "puppet",
+  "configuration" =>{
+    "environment" => "testing",
+    "server" => "puppet.example.com"
+  }
+}
+
+razor agents, 'create-broker', json do |agent|
+  step "Verify that the broker is defined on #{agent}"
+  text = on(agent, "razor -u http://#{agent}:8080/api brokers").output
+  assert_match /name:\s*"puppet-broker-test"/, text
+end
+
