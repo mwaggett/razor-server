a7723522a6864a65d5ae9ed1096281d67c1f917e
Scott McClellan <scott.mcclellan@puppetlabs.com>
2014-06-16 18:02:56 -0500

    Adding functionality to helper

--- razor_helper.rb
+++ razor_helper.rb
@@ -2,6 +2,21 @@
 require 'tmpdir'
 require 'shellwords'
 
+require 'net/ssh'
+
+# This exists to fix a bug in Net::SSH that precludes
+# the sending of unicode characters over the wire. There
+# is an issue to fix this in Beaker, but meanwhile, this
+# fix will do.
+class Net::SSH::Buffer
+  def write(*data)
+    data.each { |datum|
+      @content << datum.force_encoding('ASCII-8BIT')
+    }
+    self
+  end
+end
+
 # Collection of general helpers that get used in our tests.
 
 def reset_database(where = agents)
@@ -43,4 +58,104 @@ def razor(where, what, args = nil, options = {}, &block)
               else raise "unknown arity #{block.arity} for razor helper!"
               end
   end
+end
+
+# This method will create a policy in addition to the dependencies needed for that:
+# - Repo (creation can be bypassed by using :repo_name)
+# - Broker (creation can be bypassed by using :broker_name)
+#
+# [Optional]: This can also create and/or associate a tag.
+# => Tag creation: :create_tag = true
+# => Name/reference: :tag_name
+#
+# :task_name, :broker_type, and :policy_max_count can also be specified to alter the behavior.
+#
+# A block provided to this method will be executed inside the create-policy call, with
+# `agent` and `output` as optional parameters to the block.
+def create_policy(agents, args = {}, &block)
+  def has_key_or_default(args, key, default)
+    args.has_key?(key) ? args[key] : default
+  end
+  policy_name = has_key_or_default(args, :policy_name, 'puppet-test-policy')
+  # Use :tag_name for reference, :create_tag for creation
+  tag_name = has_key_or_default(args, :tag_name, (args[:create_tag] && 'small'))
+  repo_name = has_key_or_default(args, :repo_name, "centos-6.4")
+  broker_name = has_key_or_default(args, :broker_name, 'noop')
+  broker_type = has_key_or_default(args, :broker_type,'noop')
+  task_name = has_key_or_default(args, :task_name, "centos")
+  max_count = has_key_or_default(args, :policy_max_count, 20)
+
+  unless args[:just_policy]
+    razor agents, 'create-tag', {
+        "name" => tag_name,
+        "rule" => ["=", ["fact", "processorcount"], "2"]
+    } if args[:create_tag]
+    razor agents, 'create-repo', {
+        "name" => repo_name,
+        "url"  => 'http://provisioning.example.com/centos-6.4/x86_64/os/',
+        "task" => task_name
+    } unless args.has_key?(:repo_name)
+    razor agents, 'create-broker', {
+        "name"        => broker_name,
+        "broker-type" => broker_type
+    } unless args.has_key?(:broker_name)
+  end
+
+  json = {
+      'name'          => policy_name,
+      'repo'          => repo_name,
+      'task'          => task_name,
+      'broker'        => broker_name,
+      'enabled'       => true,
+      'hostname'      => "host${id}.example.com",
+      'root-password' => "secret",
+      'max-count'     => max_count,
+      'tags'          => tag_name.nil? ? [] : [tag_name]
+  }
+  # Workaround; max-count cannot be nil
+  json.delete('max-count') if max_count.nil?
+
+  {policy: {:name => policy_name, :max_count => max_count}, repo_name: repo_name,
+      broker: {:broker_name => broker_name, :broker_type => broker_type},
+      tag_name: tag_name, task_name: task_name}.tap do |return_hash|
+    razor agents, 'create-policy', json do |agent, output|
+      step "Verify that the policy is defined on #{agent}"
+      text = on(agent, "razor -u http://#{agent}:8080/api policies '#{policy_name}'").output
+      assert_match /#{policy_name}/, text
+      block and case block.arity
+        when 0 then yield
+        when 1 then yield agent
+        when 2 then yield agent, output
+        when 3 then yield agent, output, return_hash
+        else raise "unexpected arity #{block.arity} for create_policy!"
+      end
+    end
+  end
+end
+
+# This excludes, by default:
+# "\" => illegal character in URLs. (Encoded as %5C)
+# "'" => used as string-surrounding character on command line
+def unicode_string(length = 50, exclude = "")
+  # Why are these characters excluded? Well...
+  # - "\", "[", and "]" cause problems with the combination of URI.parse and
+  #   URI.escape. We can avoid them for these tests, but this may require
+  #   more server-side monkey-patching for proper support. As a test, try:
+  #   `URI.parse(URI.escape("http://abc.com/\\["))`
+  # - "'" causes problems with CLI, since we surround strings with
+  #   that character.
+  exclude = (exclude + "\\'[]").split(//).map(&:ord)
+  min = 32
+  max = 45295
+  (1..length).map do |index|
+    ord = rand(max - min + 1) + min
+    redo if exclude.include?(ord)
+    # Space character (32) cannot be at the start or the end of the string.
+    redo if (ord == 32 and [1, length].include?(index))
+    ord.chr('UTF-8')
+  end.join
+end
+
+def long_unicode_string(exclude = "")
+  unicode_string(250, exclude)
 end
