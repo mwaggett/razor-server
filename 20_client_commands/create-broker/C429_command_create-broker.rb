2d2a20cca92c0d09af4b45d625ea84dd75d65a7a
Scott McClellan <scott.mcclellan@puppetlabs.com>
2014-06-18 20:04:23 -0500

    Organizing existing tests
    Tests for several commands were flat in the `20_client_commands`
    folder. This refactors them into a folder for each command.

---
+++ 20_client_commands/create-broker/C429_command_create-broker.rb
@@ -0,0 +1,26 @@
+# -*- encoding: utf-8 -*-
+# this is required because of the use of eval interacting badly with require_relative
+require File.expand_path(__FILE__ + '/../../../razor_helper', '.')
+confine :to, :platform => 'el-6-x86_64'
+confine :except, :roles => %w{master dashboard database frictionless}
+
+test_name 'Command - "create-broker"'
+step 'https://testrail.ops.puppetlabs.net/index.php?/cases/view/429'
+
+reset_database
+
+json = {
+  "name"          => "puppet-test-broker",
+  "broker-type"   => "puppet",
+  "configuration" => {
+    "server"      => "puppet.example.org",
+    "environment" => "production"
+  }
+}
+
+razor agents, 'create-broker', json do |agent|
+  step "Verify that the broker is defined on #{agent}"
+  text = on(agent, "razor -u http://#{agent}:8080/api brokers").output
+  assert_match /puppet-test-broker/, text
+end
+
