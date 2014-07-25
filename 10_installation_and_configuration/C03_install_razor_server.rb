2e43187473a600391658362c4edebb461448f210
Nick Lewis <nick@puppetlabs.com>
2014-07-24 17:06:31 -0700

    (PE-5118) Update razor install step to use dir env site.pp

--- 10_installation_and_configuration/C03_install_razor_server.rb
+++ 10_installation_and_configuration/C03_install_razor_server.rb
@@ -13,13 +13,14 @@ skip_test "No available razor server hosts" if servers.empty?
 test_name 'install razor-server'
 step 'https://testrail.ops.puppetlabs.net/index.php?/cases/view/3'
 
-def with_backup_of(host, file)
+def with_backup_of(host, path)
   Dir.mktmpdir('beaker-backup') do |dir|
-    scp_from host, '/etc/puppetlabs/puppet/manifests/site.pp', dir
+    file = File.basename(path)
+    scp_from host, path, dir
     begin
       yield
     ensure
-      scp_to host, dir + '/site.pp', '/etc/puppetlabs/puppet/manifests/site.pp'
+      scp_to host, "#{dir}/#{file}", path
     end
   end
 end
@@ -31,12 +32,13 @@ step 'save the new firewall rules'
 on servers, 'service iptables save'
 
 step 'add node definitions for servers to the master'
-with_backup_of master, '/etc/puppetlabs/puppet/manifests/site.pp' do
+manifest = "#{master.puppet('master')['manifest']}/site.pp"
+with_backup_of master, manifest do
   on master, <<SH
-cat >> /etc/puppetlabs/puppet/manifests/site.pp <<EOT
+cat >> #{manifest} <<EOT
 
 # Added by 10_install_razor to test our node installing Razor
-#{servers.map {|a| "node #{a} { include pe_razor }" }.join("\n")}
+#{servers.map {|a| "node '#{a}' { include pe_razor }" }.join("\n")}
 
 EOT
 SH
