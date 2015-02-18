4029cf1dfa13c1906fb1c0e0af9ac89a2b530c16
phongdly <phong@puppetlabs.com>
2015-02-18 15:15:21 -0800

    added pe_install and config yaml file

--- acceptance/tests/test_run_scripts/create-hook/rhel6_configuration_settings.sh
+++ acceptance/tests/test_run_scripts/create-hook/rhel6_configuration_settings.sh
@@ -13,7 +13,7 @@ beaker \
   --config test_run_scripts/configs/rhel6-64mda-64a.yaml \
   --debug \
   --pre-suite 10_installation_and_configuration/00_pe_install.rb,10_installation_and_configuration/C03_install_razor_server.rb,10_installation_and_configuration/C06_install_razor_client.rb \
-  --tests 20_client_commands/configuration_settings \
+  --tests 20_client_commands/create-hook \
   --keyfile ~/.ssh/id_rsa-acceptance \
   --load-path lib \
   --preserve-hosts onfail \
