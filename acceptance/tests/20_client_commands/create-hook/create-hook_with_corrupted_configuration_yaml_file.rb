94119723039504af9771126acc905d40ef7bf100
phongdly <phong@puppetlabs.com>
2015-02-23 13:15:40 -0800

    (QA-1820) Negative tests for create-hook command
    Added negative test cases for create-hook command

---
+++ acceptance/tests/20_client_commands/create-hook/create-hook_with_corrupted_configuration_yaml_file.rb
@@ -0,0 +1,46 @@
+# -*- encoding: utf-8 -*-
+# this is required because of the use of eval interacting badly with require_relative
+require 'razor_helper'
+require 'yaml'
+confine :to, :platform => 'el-6-x86_64'
+confine :except, :roles => %w{master dashboard database frictionless}
+
+test_name 'QA-1820 - C59748 - create-hook with corrupted configuration.yaml file'
+step 'https://testrail.ops.puppetlabs.net/index.php?/cases/view/59748'
+
+hook_dir      = '/opt/puppet/share/razor-server/hooks'
+hook_type     = 'hook_type_1'
+hook_name     = 'hookName1'
+hook_path     = "#{hook_dir}/#{hook_type}.hook"
+
+teardown do
+  agents.each do |agent|
+    on(agent, "test -e #{hook_dir}.bak && mv #{hook_dir}.bak  #{hook_dir} || rm -rf #{hook_dir}")
+  end
+end
+
+step "Backup #{hook_dir}"
+agents.each do |agent|
+  on(agent, "test -e #{hook_dir} && cp #{hook_dir} #{hook_dir}.bak} || true")
+end
+
+configurationFile =<<-EOF
+---
+value:
+THIS IS A CORRUPTED CONFIGURATION.YAML FILE
+  description: "The current value of the hook"
+  default: 0
+foo:
+EOF
+
+step "Create hook type"
+agents.each do |agent|
+  on(agent, "mkdir -p #{hook_path}")
+  create_remote_file(agent,"#{hook_path}/configuration.yaml", configurationFile)
+  on(agent, "chmod +r #{hook_path}/configuration.yaml")
+  on(agent, "razor -u http://#{agent}:8080/api create-hook --name #{hook_name}" \
+            " --hook-type #{hook_type} --c value=5 --c foo=newFoo --c bar=newBar", \
+            :acceptable_exit_codes => [1]) do |result|
+    assert_match(/500 Internal Server Error/, result.stdout, 'Create hook test failed')
+  end
+end
---
+++ acceptance/tests/20_client_commands/create-hook/create-hook_with_no_hook_directory.rb
@@ -0,0 +1,34 @@
+# -*- encoding: utf-8 -*-
+# this is required because of the use of eval interacting badly with require_relative
+require 'razor_helper'
+require 'yaml'
+confine :to, :platform => 'el-6-x86_64'
+confine :except, :roles => %w{master dashboard database frictionless}
+
+test_name 'QA-1820 - C63491 - create-hook no hook directory'
+step 'https://testrail.ops.puppetlabs.net/index.php?/cases/view/63491'
+
+hook_dir      = '/opt/puppet/share/razor-server/hooks'
+hook_type     = 'hook_type_1'
+hook_name     = 'hookName1'
+hook_path     = "#{hook_dir}/#{hook_type}.hook"
+
+teardown do
+  agents.each do |agent|
+    on(agent, "test -e #{hook_dir}.bak && mv #{hook_dir}.bak  #{hook_dir} || rm -rf #{hook_dir}")
+  end
+end
+
+step "Backup #{hook_dir}"
+agents.each do |agent|
+  on(agent, "test -e #{hook_dir} && cp #{hook_dir} #{hook_dir}.bak} || true")
+end
+
+step "Create hook type"
+agents.each do |agent|
+  on(agent, "razor -u http://#{agent}:8080/api create-hook --name #{hook_name}" \
+            " --hook-type #{hook_type} --c value=5 --c foo=newFoo --c bar=newBar", \
+            :acceptable_exit_codes => [1]) do |result|
+    assert_match %r(error: hook-type must be the name of an existing hook type, but is \'#{hook_type}\'), result.stdout
+  end
+end
