e74298210f1fcd5c786863503ab1d7bf14d664f3
Scott McClellan <scott.mcclellan@puppetlabs.com>
2015-03-27 17:24:55 -0500

    (maint) Use 3.8 when installing from package for acceptance tests
    Also stop and start the service when updating the package.

--- acceptance/suites/steps/install_pe-razor-server_package.rb
+++ acceptance/suites/steps/install_pe-razor-server_package.rb
@@ -1,5 +1,7 @@
 step "Install PE Razor Server"
   razor_hosts = get_razor_hosts
   razor_hosts.each do |host|
+    on host, 'service pe-razor-server stop'
     install_pe_razor_server host
+    restart_razor_service(host)
   end
