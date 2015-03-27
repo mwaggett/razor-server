214b7b953d5d59f1c484df597a0ace3e436e47ca
Scott McClellan <scott.mcclellan@puppetlabs.com>
2015-03-27 18:48:03 -0500

    (maint) Use `curl` rather than `razor` to verify acceptance test service start

--- acceptance/suites/steps/install_pe-razor-server_package.rb
+++ acceptance/suites/steps/install_pe-razor-server_package.rb
@@ -3,5 +3,9 @@ step "Install PE Razor Server"
   razor_hosts.each do |host|
     on host, 'service pe-razor-server stop'
     install_pe_razor_server host
-    restart_razor_service(host)
+    on host, 'service pe-razor-server start >&/dev/null'
+    unless retry_on(host, 'curl -k https://localhost:8151/api',
+                    :max_retries => 30, :retry_interval => 5)
+      raise RuntimeError, "server #{host} did not start back up"
+    end
   end
